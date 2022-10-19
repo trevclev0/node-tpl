952461bcb0e8d9dd05345067da0759af6d804c90
Trevor Duran <trevclev0@gmail.com>
2022-10-19 16:58:36 -0600

    Update index.mjs
    Fixes index file

--- src/index.mjs
+++ src/index.mjs
@@ -1,32 +1,7 @@
-import dotenv from 'dotenv'
-
-const envVarExists = (envVarName) => {
-    dotenv.config({ path: '.env.example' });
-    Boolean(process.env[envVarName]);
-};
-
-const isValidEnv = (envVarName) => {
-    let retVal;
-    if (envVarExists(envVarName)) {
-        retVal = 'a';
-    }
-    else {
-        retVal = 'b';
-    }
-    if (envVarExists(envVarName)) {
-        retVal = 'c';
-    }
-    else {
-        retVal = 'd';
-    }
-
-    return retVal;
-};
-
 const awesomeDevelopers = [ 'Trevor' ];
 
 const isDevAwesome = (devName) => awesomeDevelopers.includes(devName);
 
 const developerStatus = (name) => `${name} ${isDevAwesome(name) ? 'is' : 'is NOT'} an awesome developer!`;
 
-export { developerStatus, isValidEnv };
+export { developerStatus };
